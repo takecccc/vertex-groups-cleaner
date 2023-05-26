e2bef081b059b60b890282abc90017c3c0d0ff29
takecccc <takecccc3@gmail.com>
2023-05-26 11:34:39 +0900

    pep8に従うように修正

--- __init__.py
+++ __init__.py
@@ -1,3 +1,8 @@
+import bpy
+from . import vertex_groups_cleaner
+import importlib
+importlib.reload(vertex_groups_cleaner)
+
 bl_info = {
     "name": "VertexGroupsCleaner",
     "author": "takec",
@@ -8,31 +13,28 @@ bl_info = {
     "category": "Object"
 }
 
-if "bpy" in locals():
-    import imp
-    imp.reload(vertex_groups_cleaner)
-else:
-    from . import vertex_groups_cleaner
-
-import bpy
 
 def menu_func(self, context):
     self.layout.separator()
     self.layout.operator(vertex_groups_cleaner.CleanVertexGroup.bl_idname)
 
+
 classes = [
     vertex_groups_cleaner.CleanVertexGroup,
 ]
+
 
 def register():
     for c in classes:
         bpy.utils.register_class(c)
     bpy.types.MESH_MT_vertex_group_context_menu.append(menu_func)
 
+
 def unregister():
     bpy.types.MESH_MT_vertex_group_context_menu.remove(menu_func)
     for c in classes:
         bpy.utils.unregister_class(c)
+
 
 if __name__ == "__main__":
     register()
